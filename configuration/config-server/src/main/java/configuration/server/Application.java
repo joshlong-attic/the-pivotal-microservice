82d3fee3240e59e7fc417ac4584de1c4bbd4c6a8
Josh Long <josh@joshlong.com or josh.long@springsource.com>
2014-11-11 01:18:00 -0800

    introducing config-server,client

---
+++ configuration/config-server/src/main/java/configuration/server/Application.java
@@ -0,0 +1,16 @@
+package configuration.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableConfigServer
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
